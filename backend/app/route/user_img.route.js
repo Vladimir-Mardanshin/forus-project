6675f8f036198faba8abf6caba415c8fead43d06
VladimirMardanshin <vova_mardanshin@mail.ru>
2023-12-23 00:35:30 +0800

    Новый

--- backend/app/route/user_img.route.js
+++
@@ -1,8 +0,0 @@
-module.exports = (app) => {
-
-    const user_img = require('../controller/user_img.controller');
-    var { authJwt } = require("../middleware");
-
-    app.post('/api/addUserImg',  [authJwt.verifyToken],user_img.addUserImg);
-    app.get('/api/getUserImgById',  user_img.getUserImgById);
-};
