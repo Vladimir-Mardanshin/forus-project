1ad5feff4e25bdc1580d7ba8008e2ffdbcbdbdeb
Bah1i <98271293+Bah1i@users.noreply.github.com>
2023-12-23 12:00:34 +0800

    Delete backend directory

--- backend/app/route/user.route.js
+++
@@ -1,9 +0,0 @@
-module.exports = (app) => {
-
-    const user = require('../controller/user.controller');
-    var { authJwt } = require("../middleware");
-
-    app.get("/api/findUserById/:id", [authJwt.verifyToken], user.findById);
-
-    app.post("/api/findExecutors", user.findExecutors);
-};
